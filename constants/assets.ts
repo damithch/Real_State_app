b383b17ecf1f392952351d1ad79c0a43329866b4
Damith Chandrathilake <161125812+damithch@users.noreply.github.com>
2025-08-04 17:39:54 +0530

    added the neccessary assests

---
+++ constants/assets.ts
@@ -0,0 +1,68 @@
+// Asset constants for easy import management
+export const Icons = {
+  // Navigation & UI
+  home: require('../assets/icons/home.png'),
+  search: require('../assets/icons/search.png'),
+  heart: require('../assets/icons/heart.png'),
+  person: require('../assets/icons/person.png'),
+  filter: require('../assets/icons/filter.png'),
+  bell: require('../assets/icons/bell.png'),
+  chat: require('../assets/icons/chat.png'),
+  
+  // Property Features
+  bed: require('../assets/icons/bed.png'),
+  bath: require('../assets/icons/bath.png'),
+  area: require('../assets/icons/area.png'),
+  carPark: require('../assets/icons/car-park.png'),
+  wifi: require('../assets/icons/wifi.png'),
+  swim: require('../assets/icons/swim.png'),
+  dumbell: require('../assets/icons/dumbell.png'),
+  laundry: require('../assets/icons/laundry.png'),
+  dog: require('../assets/icons/dog.png'),
+  
+  // Actions
+  location: require('../assets/icons/location.png'),
+  phone: require('../assets/icons/phone.png'),
+  send: require('../assets/icons/send.png'),
+  edit: require('../assets/icons/edit.png'),
+  backArrow: require('../assets/icons/back-arrow.png'),
+  rightArrow: require('../assets/icons/right-arrow.png'),
+  
+  // Auth & Social
+  google: require('../assets/icons/google.png'),
+  shield: require('../assets/icons/shield.png'),
+  logout: require('../assets/icons/logout.png'),
+  
+  // Misc
+  star: require('../assets/icons/star.png'),
+  calendar: require('../assets/icons/calendar.png'),
+  wallet: require('../assets/icons/wallet.png'),
+  run: require('../assets/icons/run.png'),
+  cutlery: require('../assets/icons/cutlery.png'),
+  people: require('../assets/icons/people.png'),
+  language: require('../assets/icons/language.png'),
+  info: require('../assets/icons/info.png'),
+};
+
+export const Images = {
+  // App branding
+  logo: require('../assets/images/icon.png'),
+  splash: require('../assets/images/splash.png'),
+  onboarding: require('../assets/images/onboarding.png'),
+  
+  // UI Elements
+  avatar: require('../assets/images/avatar.png'),
+  noResult: require('../assets/images/no-result.png'),
+  cardGradient: require('../assets/images/card-gradient.png'),
+  whiteGradient: require('../assets/images/white-gradient.png'),
+  barChart: require('../assets/images/bar-chart.png'),
+  
+  // Locations
+  japan: require('../assets/images/japan.png'),
+  newYork: require('../assets/images/new-york.png'),
+  map: require('../assets/images/map.png'),
+  
+  // App icons
+  favicon: require('../assets/images/favicon.png'),
+  splashIcon: require('../assets/images/splash-icon.png'),
+};
