6921bc19cf4b7b2df4bf7bf1fa7e18f28f8be226
Damith Chandrathilake <161125812+damithch@users.noreply.github.com>
2025-08-04 15:51:01 +0530

    configured the tialwaind and native wind css styles usage

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,14 +3,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      }}
-    >
-      <Text className="text-4xl font-bold text-blue-600 mb-8 text-center">WELCOME</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-4xl font-black text-blue-600 mb-8 text-center">
+        WELCOME
+      </Text>
       <Link href="../../sign-in">
         <Text>SIGN IN</Text>
       </Link>
